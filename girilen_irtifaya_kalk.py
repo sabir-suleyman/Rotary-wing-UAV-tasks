bd91eee5c57f1a9dc37db94e8b3c7c7d7b346155
Sabir Süleymanlı <81561786+sabir-suleyman@users.noreply.github.com>
2023-06-10 11:56:07 +0300

    Girilen irtifaya kalk

---
+++ girilen_irtifaya_kalk.py
@@ -0,0 +1,33 @@
+from dronekit import connect , VehicleMode
+import time
+
+iha = connect("127.0.0.1:14550", wait_ready= True)
+
+def takeof(irtifa1):
+
+    while iha.is_armable is not True:
+        print("Iha armedilebilir durumda degil...")
+        
+        
+    print("Iha armedilebilir.")
+            
+            
+    iha.mode = VehicleMode("GUIDED")
+            
+    print("Iha Guided moduna alindi.")
+
+    iha.armed = True
+
+    print("Iha basariyla arm edildi!")
+
+    iha.simple_takeoff(irtifa1)
+
+    while iha.location.global_relative_frame.alt < irtifa1*0.9:
+        print("Iha Beirtilen yukseklige kalkiyor...")
+        time.sleep(1)
+    
+    print("Iha istenilen irtifaya ulasti")
+
+irtifa = int(input("Kalkilacak yukseklik degerini giriniz: "))
+
+takeof(irtifa)
