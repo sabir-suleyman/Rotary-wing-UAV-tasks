224361848c0c29ea0ba039c9b018b2278085bd09
Sabir Süleymanlı <81561786+sabir-suleyman@users.noreply.github.com>
2023-06-10 13:29:10 +0400

    Görev oluşturma

--- gorev_olustur_1.py
+++ gorev_olustur_1.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+#
+#       Sabir Süleymanlı <suleymanlisabir3@gmail.com>
+#       
+#       gorev_olustur_1.py            05.07.2022           
+#       
+
 from tokenize import Double
 from dronekit import Command ,connect , VehicleMode, LocationGlobalRelative
 import time
